a984e2ea622b5fd95657b87e46b9e3c565e425b5
SREEDHAR <sri91507@gmail.com>
2025-10-06 23:34:38 +0530

    Update for Vercel deployment

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -11,11 +11,11 @@ const users = [
 
 const customers = [
   {
-    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
-  },
+  id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+  name: 'Friendly Fox',
+  email: 'evil@rabbit.com',
+  image_url: '/customers/evil-rabbit.png',
+},
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
     name: 'Delba de Oliveira',
@@ -30,7 +30,7 @@ const customers = [
   },
   {
     id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-    name: 'Michael Novotny',
+    name: 'yOGESH',
     email: 'michael@novotny.com',
     image_url: '/customers/michael-novotny.png',
   },
