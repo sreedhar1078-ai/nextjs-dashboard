310691fa47797e6c66abbdb760444387e85b3d58
SREEDHAR <sri91507@gmail.com>
2025-10-15 16:33:03 +0530

    Update feature X and fix bug Y

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,14 +1,10 @@
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import SignOutButton from '@/app/ui/dashboard/signout-button';
-
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-
-        <SignOutButton />
       </div>
     </div>
   );
